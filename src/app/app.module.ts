5c5fd464a190536f2c53610806e6622202addc98
allan-mlpe <allan_mlpe@hotmail.com>
2018-02-09 18:06:26 -0300

    Configuração básica da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './views/navbar/navbar.component';
+import { IntroProjectComponent } from './views/intro-project/intro-project.component';
+import { TeamsComponent } from './views/teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    IntroProjectComponent,
+    TeamsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
